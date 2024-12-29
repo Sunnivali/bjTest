6e2071150bb4b4600da6beff5719245c9cbfcba5
Sunnivali <sunniva.lien@gmail.com>
2024-12-29 11:18:37 +0100

    add all files to git

---
+++ Car.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Car here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Car
+{
+    // instance variables - replace the example below with your own
+    private int year;
+    private String color;
+
+    /**
+     * Constructor for objects of class Car
+     */
+    public Car()
+    {
+        // initialise instance variables
+        year = 2024;
+        color = "white";
+    }
+
+    /**
+     * Set the color of the car
+     *
+     * @param  col  the new color
+     * @return The car color
+     */
+    public void setColor(String col)
+    {
+        // put your code here
+        color = col;
+    }
+    
+    public String getColor()
+    {
+        return color;
+    }  
+    
+    public int getYear()
+    {
+        return year;
+    } 
+}
